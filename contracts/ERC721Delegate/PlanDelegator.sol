854174e9e40c379bd8ef68ce2d33162413aadd66
iceman <acfmichelsen@gmail.com>
2024-05-29 21:50:13 -0400

    lock pragmas

--- contracts/ERC721Delegate/PlanDelegator.sol
+++ contracts/ERC721Delegate/PlanDelegator.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.20;
+pragma solidity 0.8.24;
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
 
 abstract contract PlanDelegator is ERC721Enumerable {
